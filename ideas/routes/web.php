94eda903a90128e0bebada5271f7fd1270a4d9e6
staz <m.kobialka@healthpharm.pl>
2024-02-07 21:05:38 +0100

    Created Views and controllers

--- ideas/routes/web.php
+++ ideas/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', );
+Route::get('/', [ DashboardController::class, 'index' ]);
+
+Route::get('/profile', [ ProfileController::class, 'index' ]);
 
 
 

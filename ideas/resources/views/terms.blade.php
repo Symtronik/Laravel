98fbdd8356af6bb018ab623522f8bb6c66a4c528
staz <m.kobialka@healthpharm.pl>
2024-02-07 22:20:11 +0100

    Created layouts

---
+++ ideas/resources/views/terms.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.layout')
+
+@section('content')
+    <h1>Terms</h1>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aliquid quaerat laudantium unde, quisquam rem
+    vitae deserunt inventore aperiam beatae asperiores adipisci maiores tempore molestias alias nulla vero possimus
+    magni totam enim excepturi in ipsam. Vel tempora mollitia iste accusamus aut nobis maxime, aliquid, repudiandae,
+    ratione quaerat perspiciatis tempore iure.</div>
+
+@endsection
+
+

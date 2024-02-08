7db08e2173e5d293f4c80b5a2864d626b821901a
staz <m.kobialka@healthpharm.pl>
2024-02-08 16:48:07 +0100

    Added new views

--- ideas/routes/web.php
+++ ideas/routes/web.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ DashboardController::class, 'index' ]);
 
+Route::post('/idea', [ DashboardController::class, 'index' ]) ->name('idea.create');
+
 Route::get('/terms', function(){
     return view('terms');
 });
